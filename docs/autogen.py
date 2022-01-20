7e568c9666160ec6e53090b087ee3587b3ce5cfe
justingoheen <26209687+justingoheen@users.noreply.github.com>
2022-01-20 14:13:39 -0500

    updating autogen

---
+++ docs/autogen.py
@@ -0,0 +1,17 @@
+import keras_autodoc
+import pathlib
+import shutil
+
+PAGES = {"The Algorithm.md": ["convtasnet.algorithm.ConvTasNet"]}
+
+root_dir = pathlib.Path(__file__).resolve().parents[1]
+
+
+def generate(dest_dir):
+    doc_generator = keras_autodoc.DocumentationGenerator(PAGES)
+    doc_generator.generate(dest_dir)
+    shutil.copyfile(root_dir / "README.md", dest_dir / "index.md")
+
+
+if __name__ == "__main__":
+    generate(root_dir / "docs" / "sources")
